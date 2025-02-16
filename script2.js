9bf075c4d865f57966fd55f2f8dafcbf16bba1da
Ani <158804664+an619@users.noreply.github.com>
2025-02-16 20:15:25 +0400

    GOA

--- script2.js
+++ script2.js
@@ -1,27 +1,35 @@
 const booksDiv = document.querySelector('.books')
 
 const library = JSON.parse(localStorage.getItem('library')) || [];
-library.forEach(book => {
-    console.log(book.img)
-    booksDiv.innerHTML += `
-         <div class="book">
-                <img src=${book.img} alt=${book.title}>
-                <h2>${book.title}</h2>
-                <p>author: <span>${book.author}</span></p>
-                <p>pages: <span>${book.pages}</span></p>
-                <button id=${book.id} onclick=${deleteFromLC(book)}>delete</button>
-        </div>`})
 
-function deleteFromLC(obj) {
+function render() {
+    library.forEach(book => {
+        console.log(book.img)
+        booksDiv.innerHTML += `
+             <div class="book">
+                    <img src=${book.img} alt=${book.title}>
+                    <h2>${book.title}</h2>
+                    <p>author: <span>${book.author}</span></p>
+                    <p>pages: <span>${book.pages}</span></p>
+                    <button id=${book.id} onclick=${deleteFromLC(book.id)}>delete</button>
+            </div>`})
+}
+
+render();
+
+function deleteFromLC(objId) {
+    
     const library = JSON.parse(localStorage.getItem('library')) || [];
     const result = [];
     for(let book of library){
-        if(JSON.stringify(book) != JSON.stringify(obj)){
+        if(book.id !== objId){
             result.push(book);
-            console.log(book);
+            console.log(book)
+            render();
         }
     }
     console.log(result)
     localStorage.setItem('library', JSON.stringify(result));
+    render()
 }
 
